2eb5e5d06bee3e61ee279514056a640542aef5f2
JinHyeong Park <pjhyng0125@naver.com>
2023-08-22 23:58:26 +0900

    category html, js 추가

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,24 @@ $(function () {
   // include html files
   $(".gnb-include").load("/include/gnb.html");
   $(".main-header-include").load("/include/main-header.html");
+
+  // Category Accordion
+  $(".category-accordion .detail").eq(0).show();
+
+  $(".category-accordion .title").click(function () {
+    // 펼치기
+    // $(this).next().show();
+    // $(this).next().stop().slideDown();
+    // $(this).next().stop().fadeIn();
+
+    // 펼치고 접기
+    // $(this).next().stop().slideToggle();
+
+    // 하나만 펼치기
+    $(this).next().stop().slideDown();
+    $(this).siblings(".category-accordion .title").next().stop().slideUp();
+
+    $(this).addClass("active");
+    $(this).siblings(".category-accordion .title").removeClass("active");
+  });
 });
